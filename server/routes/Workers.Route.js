bb347369ad2b2218810f68c9ec0d0bf73c67fbb4
Shadi Rada <shakkyr@gmail.com>
2022-06-04 23:51:23 +0300

    adding workers to a specific task

--- server/routes/Workers.Route.js
+++ server/routes/Workers.Route.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const {Workers} = require('../models')
+const {Workers} = require('../models');
 
+router.get('/:taskId', async (req, res) => {
+    const taskId = req.params.taskId;
+    const workers = await Workers.findAll({ where: {TaskId: taskId}});
+    res.json(workers);
+})
 
-
+router.post('/', async (req, res) => {
+    const worker = req.body
+    await Workers.create(worker);
+    res.json(worker);
+})
 
 module.exports = router;
